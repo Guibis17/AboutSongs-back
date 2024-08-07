6efa86e1f7d319942901568dc0507104d70ea3fa
Guibis17 <guilherme19.b07@gmail.com>
2024-08-07 10:56:20 -0300

    Mudanças no controller

--- AboutSongs/Controllers/HomeController.cs
+++ AboutSongs/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ public class HomeController : Controller
     {
         return View();
     }
-    public IActionResult Inscrever()
+    public IActionResult Entrar()
     {
         return View();
     }
