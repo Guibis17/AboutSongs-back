46356d197770677a0981fe4d77d58e6f6cd51a49
BrunoOller <brunobrunelli4@gmail.com>
2024-11-29 12:12:17 -0300

    Atualizando o projeto

--- AboutSongs/Controllers/AccountController.cs
+++ AboutSongs/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Security.Policy;
 using AboutSongs.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using AboutSongs.Services;
 
 
 namespace AboutSongs.Controllers;
@@ -12,6 +13,7 @@ public class AccountController : Controller
     private readonly ILogger<AccountController> _logger;
     private readonly SignInManager<IdentityUser> _signInManager;
     private readonly UserManager<IdentityUser> _userInManager;
+    private readonly IUsuarioService _usuarioService;
 
     public AccountController(
         ILogger<AccountController> logger,
@@ -67,6 +69,31 @@ public class AccountController : Controller
             ModelState.AddModelError(string.Empty, "Usuário e/ou Senha Inválidos!!!");
         }
         return View(login);
+    }
+
+    [HttpGet]
+    public IActionResult Registro()
+    {
+        RegistroVM register = new();
+        return View(register);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Registro(RegistroVM register)
+    {
+        register.Enviado = false;
+        if (ModelState.IsValid)
+        {
+            var result = await _usuarioService.RegistrarUsuario(register);
+            if (result != null)
+                foreach (var error in result)
+                {
+                    ModelState.AddModelError(string.Empty, error);
+                }
+            register.Enviado = result == null;
+        }
+        return View(register);
     }
 
     [HttpPost]
